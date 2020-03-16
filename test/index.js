7bb4266fe40e419009b97dac0cc42f5fda712561
Jonas Hermsmeier <jhermsmeier@gmail.com>
2020-03-16 16:46:29 +0100

    test: Remove previous tests

--- test/index.js
+++ test/index.js
@@ -1,80 +1,25 @@
-var BlockDevice = require( 'blockdevice' )
-var Disk = require( 'disk' )
-var ExFAT = require( '..' )
 var assert = require( 'assert' )
 var path = require( 'path' )
 var fs = require( 'fs' )
+var stream = require( 'stream' )
 var zlib = require( 'zlib' )
+var ExFAT = require( '..' )
+var inspect = require( './inspect' )
 
-var log = console.log.bind( console )
-var inspect = function( value ) {
-  return require( 'util' ).inspect( value, {
-    colors: true,
-    depth: null,
-  })
-}
-
-suite( 'ExFAT', function() {
+describe( 'ExFAT', function() {
   
-  var device = null
-  var disk = null
-  var partition = null
-  var volume = null
-  
-  suiteSetup( 'Decompress image', function( next ) {
+  before( 'Decompress image', function( done ) {
     
     var source = path.join( __dirname, 'data', 'exfat.img.gz' )
     var destination = path.join( __dirname, 'data', 'exfat.img' )
+    var streams = [
+      fs.createReadStream( source ),
+      zlib.createGunzip(),
+      fs.createWriteStream( destination )
+    ]
     
-    fs.createReadStream( source )
-      .pipe( zlib.createGunzip() )
-      .pipe( fs.createWriteStream( destination ) )
-      .on( 'finish', next )
+    stream.pipeline( ...streams, done )
     
-  })
-  
-  suiteSetup( 'Create BlockDevice', function() {
-    device = new BlockDevice({
-      path: path.join( __dirname, 'data', 'exfat.img' ), // 'usb-thumb-exfat.bin'
-      mode: 'r',
-      blockSize: 512,
-    })
-  })
-  
-  suiteSetup( 'Open Disk', function( done ) {
-    disk = new Disk( device )
-    disk.open( function( error ) {
-      if( error ) console.log( 'ERROR', error.message )
-      // log( inspect( disk ) )
-      // log( '' )
-      done()
-    })
-  })
-  
-  test( 'Instance ExFAT Volume', function( done ) {
-    
-    log( inspect( disk ) )
-    log( '' )
-    
-    var part = disk.gpt.partitions[0]
-    
-    partition = device.partition({
-      firstLBA: part.firstLBA,
-      lastLBA: part.lastLBA,
-    })
-    
-    volume = new ExFAT.Volume( partition )
-    
-    volume.mount( function( error ) {
-      log( inspect( volume ) )
-      log( '' )
-      done( error )
-    })
-    
-  })
-  
-  suiteTeardown( 'Close Disk', function( done ) {
-    disk.close( done )
   })
   
 })
